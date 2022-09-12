5a0956d6ac8dc913598f246f22661e704f304893
Tobias <tobiaslamanna@gmail.com>
2022-09-12 07:25:51 -0300

    subir tp

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use std::env;
+use std::fs;
+mod matriz_buscaminas;
+
+fn main() {
+    let result_tablero_string = abrir_archivo();
+    let tablero_string = match result_tablero_string {
+        Ok(contenido_archivo) => contenido_archivo,
+        Err(error) => {
+            print!("No se pudo abrir el archivo: {}", error);
+            return;
+        }
+    };
+    let tablero_bytes = tablero_string.as_bytes();
+
+    let mut tablero_matriz = matriz_buscaminas::MatrizBuscaminas::new();
+    tablero_matriz.popular_desde_bytes(tablero_bytes);
+    tablero_matriz.contar_bombas();
+    tablero_matriz.imprimir_como_buscaminas();
+}
+
+/// Abre el archivo indicado como argumento al ejecutar el programa.
+/// El argumento debe ser el path al archivo incluyendo el nombre del archivo
+/// desde la carpeta raiz en la que se ejecute el programa.
+/// 
+/// Devuelve un Result<String, Error>, siendo el String el contenido del archivo y el Error el devuelto por fs::read_to_string.
+/// 
+/// #Ejemplo
+/// 
+/// ```
+/// let contenido_result = abrir_archivo();
+/// let contenido = match contenido_result {
+///     Ok(contenido_archivo) => contenido_archivo,
+///     Err(error) => {
+///         print!("No se pudo abrir el archivo: {}", error);
+///         return;
+///     }
+/// };
+/// ```
+fn abrir_archivo() -> Result<String, std::io::Error> {
+    let args: Vec<String> = env::args().collect();
+    let archivo = &args[1];
+    fs::read_to_string(archivo)
+}
---
+++ src/matriz_buscaminas.rs
@@ -0,0 +1,295 @@
+/// Estructura que representa una matriz, con metodos específicos para ser utilizada
+/// como tablero del juego buscaminas.
+pub struct MatrizBuscaminas {
+    valores: Vec<i32>,
+    columnas: i32,
+    filas: i32,
+}
+
+/// Constante que representa el valor del byte del caracter '*' en ASCII
+const ASTERISCO_BYTE: u8 = b'*';
+
+/// Constante que representa el valor del  byte del caracter '·' en ASCII
+const INTERDOT_BYTE: u8 =  b'\xB7';
+
+impl MatrizBuscaminas {
+    /// Devuelve una MatrizBuscaminas vacia.
+    pub fn new() -> MatrizBuscaminas {
+        MatrizBuscaminas {
+            columnas: 0,
+            filas: 0,
+            valores: vec![0; 0],
+        }
+    }
+
+    /// Metodo público que permite llenar una matriz vacia a partir de un arreglo
+    /// de u8 compuesto por '*' representando bombas y '·' representando celdas vacias.
+    /// Tambien genera una fila por cada salto de linea.
+    /// 
+    /// # Argumentos
+    ///
+    /// * `bytes` - Arreglo de u8 con los caracteres que representan un juego de buscaminas.
+    /// 
+    /// #Ejemplo
+    /// 
+    /// ```
+    /// let bytes = [b'*', b'\xB7', b'*' , b'\xB7', b'\n']
+    /// let mut matriz = MatrizBuscaminas::new();
+    /// matriz.popular_desde_bytes(bytes);
+    /// ```
+    pub fn popular_desde_bytes(&mut self, bytes: &[u8]) {
+        self.columnas = Self::contar_columnas(bytes);
+        self.filas = Self::contar_filas(bytes);
+
+        for byte in bytes {
+            if *byte == ASTERISCO_BYTE {
+                self.valores.push(-1)
+            }
+            if *byte == INTERDOT_BYTE {
+                self.valores.push(0)
+            }
+        }
+    }
+
+    /// Metodo público que permite completar las celdas vacias de una MatrizBuscaminas con el número correspondiente
+    /// segun la cantidad de bombas adyacentes.
+    /// 
+    /// #Ejemplo
+    /// 
+    /// ```
+    /// matriz.contar_bombas();
+    /// ```
+    pub fn contar_bombas(&mut self) {
+        for i in 0..self.valores.len() {
+            if self.valores[i] == -1 {
+                Self::aumentar_adyacentes(self, i as i32);
+            }
+        }
+    }
+
+    /// Metodo público que permite imprimir por salida estandar una MatrizBuscaminas
+    /// con el formato del tablero de buscaminas.
+    /// 
+    /// #Ejemplo
+    /// 
+    /// ```
+    /// let bytes = [b'*', b'\xB7', b'*' , b'\xB7', b'\n']
+    /// let mut matriz = MatrizBuscaminas::new();
+    /// matriz.popular_desde_bytes(&bytes);
+    /// matriz.contar_bombas();
+    /// matriz.imprimir_como_buscaminas();
+    /// ```
+    /// #Salida
+    /// *2*1
+
+    pub fn imprimir_como_buscaminas(&self) {
+        let mut contador_columnas = 0;
+        for valor in &self.valores {
+            if contador_columnas == self.columnas {
+                println!();
+                contador_columnas = 0;
+            }
+            if *valor == -1 {
+                print!("*");
+            } else if *valor == 0 {
+                print!("·");
+            } else {
+                print!("{}", *valor);
+            }
+            contador_columnas += 1;
+        }
+        println!();
+    }
+
+    /// Función interna del módulo.
+    /// Cuenta la cantidad de columnas de un arreglo de u8 a partir de la 
+    /// cantidad de caracteres hasta el primer salto de linea.
+    fn contar_columnas(bytes: &[u8]) -> i32 {
+        let mut columnas = 0;
+        for byte in bytes {
+            if *byte == (b'\n') {
+                break;
+            } else if *byte == INTERDOT_BYTE || *byte == ASTERISCO_BYTE {
+                columnas += 1;
+            }
+        }
+        columnas
+    }
+
+    /// Función interna del módulo.
+    /// Cuenta la cantidad de filas de un arreglo de u8 a partir de la 
+    /// cantidad de saltos de linea.
+    fn contar_filas(bytes: &[u8]) -> i32 {
+        let mut filas = 0;
+        for byte in bytes {
+            if *byte == (b'\n') {
+                filas += 1;
+            }
+        }
+        filas
+    }
+
+    /// Función interna del módulo.
+    /// Aumenta en 1 el valor de las celdas adyacentes a una celda dada por el
+    /// indice en el vector valores de la MatrizBuscaminas. 
+    fn aumentar_adyacentes(&mut self, i: i32) {
+        let coord = Self::obtener_coordenadas(i, self.columnas);
+        let celdas_a_aumentar = [
+            (coord.0 - 1, coord.1 - 1),
+            (coord.0 - 1, coord.1),
+            (coord.0 - 1, coord.1 + 1),
+            (coord.0, coord.1 - 1),
+            (coord.0, coord.1 + 1),
+            (coord.0 + 1, coord.1 - 1),
+            (coord.0 + 1, coord.1),
+            (coord.0 + 1, coord.1 + 1),
+        ];
+        for celda in celdas_a_aumentar {
+            Self::aumentar_celda(self, celda);
+        }
+    }
+
+    /// Función interna del módulo.
+    /// Aumenta en 1 el valor de la celda indicada en caso que se encuentre dentro del
+    /// tablero y no sea una celda con una mina. 
+    fn aumentar_celda(&mut self, celda: (i32, i32)) {
+        if celda.0 < 0 || celda.0 >= self.filas || celda.1 < 0 || celda.1 >= self.columnas {
+            return;
+        }
+        let posicion = celda.0 * self.columnas + celda.1;
+        if self.valores[posicion as usize] != -1 {
+            self.valores[posicion as usize] += 1;
+        }
+    }
+
+    /// Función interna del módulo.
+    /// Devuelve las coordenadas que tendria una celda en una representacion matricial
+    /// a partir del indice en el vector valores de la MatrizBuscaminas.
+    fn obtener_coordenadas(i: i32, col: i32) -> (i32, i32) {
+        let fila = i / col;
+        let columna = i % col;
+        (fila, columna)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_crea_matriz_vacia() {
+        let matriz = MatrizBuscaminas::new();
+        assert_eq!(matriz.columnas, 0);
+        assert_eq!(matriz.filas, 0);
+        assert_eq!(matriz.valores.len(), 0);
+    }
+
+    #[test]
+    fn test_popular_desde_bytes_agrega_menos_uno_si_encuentra_asterisco() {
+        let mut matriz = MatrizBuscaminas::new();
+        let bytes = [ASTERISCO_BYTE];
+        matriz.popular_desde_bytes(&bytes);
+        assert_eq!(matriz.valores.len(), 1);
+        assert_eq!(matriz.valores[0], -1);
+    }
+
+    #[test]
+    fn test_popular_desde_bytes_agrega_cero_si_encuentra_punto() {
+        let mut matriz = MatrizBuscaminas::new();
+        let bytes = [INTERDOT_BYTE];
+        matriz.popular_desde_bytes(&bytes);
+        assert_eq!(matriz.valores.len(), 1);
+        assert_eq!(matriz.valores[0], 0);
+    }
+
+    #[test]
+    fn test_popular_desde_bytes_agrega_fila_si_encuentra_salto_de_linea() {
+        let mut matriz = MatrizBuscaminas::new();
+        let bytes = [b'\n', b'\n', b'\n'];
+        matriz.popular_desde_bytes(&bytes);
+        assert_eq!(matriz.valores.len(), 0);
+        assert_eq!(matriz.filas, 3);
+    }
+
+    #[test]
+    fn test_popular_desde_bytes_agrega_columnas_si_no_encuentra_salto_de_linea() {
+        let mut matriz = MatrizBuscaminas::new();
+        let bytes = [INTERDOT_BYTE, INTERDOT_BYTE];
+        matriz.popular_desde_bytes(&bytes);
+        assert_eq!(matriz.valores.len(), 2);
+        assert_eq!(matriz.columnas, 2);
+    }
+
+    #[test]
+    fn test_contar_bombas_suma_adyacentes_horizontales() {
+        let mut matriz = MatrizBuscaminas::new();
+        let bytes = [INTERDOT_BYTE, ASTERISCO_BYTE, INTERDOT_BYTE, b'\n'];
+        matriz.popular_desde_bytes(&bytes);
+        matriz.contar_bombas();
+        assert_eq!(matriz.valores[0], 1);
+        assert_eq!(matriz.valores[2], 1);
+    }
+
+    #[test]
+    fn test_contar_bombas_suma_adyacentes_verticales() {
+        let mut matriz = MatrizBuscaminas::new();
+        let bytes = [
+            INTERDOT_BYTE,
+            b'\n',
+            ASTERISCO_BYTE,
+            b'\n',
+            INTERDOT_BYTE,
+            b'\n',
+        ];
+        matriz.popular_desde_bytes(&bytes);
+        matriz.contar_bombas();
+        assert_eq!(matriz.valores[0], 1);
+        assert_eq!(matriz.valores[2], 1);
+    }
+
+    #[test]
+    fn test_contar_bombas_suma_adyacentes_diagonales() {
+        let mut matriz = MatrizBuscaminas::new();
+        let bytes = [
+            INTERDOT_BYTE,
+            INTERDOT_BYTE,
+            b'\n',
+            ASTERISCO_BYTE,
+            INTERDOT_BYTE,
+            b'\n',
+            INTERDOT_BYTE,
+            INTERDOT_BYTE,
+            b'\n',
+        ];
+        matriz.popular_desde_bytes(&bytes);
+        matriz.contar_bombas();
+        assert_eq!(matriz.valores[1], 1);
+        assert_eq!(matriz.valores[5], 1);
+    }
+
+    #[test]
+    fn test_contar_bombas_suma_corectamente_dos_bombas() {
+        let mut matriz = MatrizBuscaminas::new();
+        let bytes = [ASTERISCO_BYTE, INTERDOT_BYTE, ASTERISCO_BYTE, b'\n'];
+        matriz.popular_desde_bytes(&bytes);
+        matriz.contar_bombas();
+        assert_eq!(matriz.valores[1], 2);
+    }
+
+    #[test]
+    fn test_contar_bombas_suma_corectamente_tres_bombas() {
+        let mut matriz = MatrizBuscaminas::new();
+        let bytes = [
+            ASTERISCO_BYTE,
+            INTERDOT_BYTE,
+            ASTERISCO_BYTE,
+            b'\n',
+            INTERDOT_BYTE,
+            ASTERISCO_BYTE,
+            INTERDOT_BYTE,
+        ];
+        matriz.popular_desde_bytes(&bytes);
+        matriz.contar_bombas();
+        assert_eq!(matriz.valores[1], 3);
+    }
+}
